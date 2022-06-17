60420f8a18244717478cc1612ee4dd381e67c151
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-17 22:45:31 +0900

    (Modified:) Set up Semantic-UI for Rails7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,11 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "jquery"
 
+// サイト全体で ”Turbo Drive” を無効化する
+// data: { turbo: true, ... } とすることて必要に応じて使用可能
+Turbo.session.drive = false;
+
+
 
 /* jQuery for Semantic UI  */
 $(function(){
