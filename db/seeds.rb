54a2067bd222d95c3f178c02d8a2cfabc356a48c
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-18 17:49:41 +0900

    Complete Chatroom, login page (add some controllers, models)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(body: "コンニチハ！！", user: User.first)
+Message.create(body: "コンニチハ！！", user: User.second)
+Message.create(body: "よろぴく！！", user: User.third)
